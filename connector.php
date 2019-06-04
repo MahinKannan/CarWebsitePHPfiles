ef11620c419b0455735accaae097a9442c6d47de
Mahin Kannan <1mahink@gmail.com>
2019-06-03 20:54:10 -0500

    Update connector.php

--- connector.php
+++ connector.php
@@ -1,9 +1,9 @@
 <?php
 function connect(){
-$servername = "handson-mysql";
-$username="guestinterns";
-$password="interns2017";
-$dbname= "intern2017_group4";
+$servername = "";
+$username="";
+$password="";
+$dbname= "";
 return $conn= mysqli_connect($servername, $username, $password, $dbname);
 }
 ?>
